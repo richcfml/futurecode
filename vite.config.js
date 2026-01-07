cbc00cf3c28c129cc5f175604cb83832c9b1d37f
Richard Ardila <richcfml@gmail.com>
2026-01-07 15:13:21 -0500

    Update static site with new page

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
 import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "node:path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [tailwindcss()],
   base: "/",
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    sourcemap: true,
+    minify: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        services: resolve(__dirname, "services.html"),
+      },
+      output: {
+        chunkFileNames: "assets/[name]-[hash].js",
+        entryFileNames: "assets/[name]-[hash].js",
+        assetFileNames: "assets/[name]-[hash].[ext]",
+      },
+    },
+  },
 });
